1e905d315f124525b32467bedb2fc738e717f264
Sergey Laptev <iamsergeylaptev@gmail.com>
2015-08-04 01:43:48 +0300

    EntryBricksContainer page system [deliver #100309990]

--- app/scripts/desktop/react/components/EntryBricks/EntryBricksContainer.jsx
+++ app/scripts/desktop/react/components/EntryBricks/EntryBricksContainer.jsx
@@ -1,65 +1,58 @@
+import React, { Component, PropTypes } from 'react';
 import EntryActionCreators from '../../actions/Entry';
 import EntryBricks from './EntryBricks';
 
-let EntryBricksContainer = React.createClass({
-  propTypes: {
-    entries_info: React.PropTypes.shape({
-      items: React.PropTypes.array.isRequired,
-      limit: React.PropTypes.number.isRequired,
-      has_more: React.PropTypes.bool.isRequired,
-      next_since_entry_id: React.PropTypes.number
+export default class EntryBricksContainer extends Component {
+  static propTypes = {
+    entries_info: PropTypes.shape({
+      items: PropTypes.array.isRequired,
+      limit: PropTypes.number.isRequired,
+      has_more: PropTypes.bool,
+      next_page: PropTypes.number,
+      next_since_entry_id: PropTypes.number
     }).isRequired,
-    loadUrl: React.PropTypes.string.isRequired
-  },
-
-  getInitialState() {
-    return {
-      entries: this.props.entries_info.items,
-      hasMore: this.props.entries_info.has_more,
-      sinceEntryID: this.props.entries_info.next_since_entry_id,
-      loading: false
-    };
-  },
-
+    loadUrl: PropTypes.string.isRequired,
+    nextPageFieldName: PropTypes.oneOf(['next_page', 'next_since_entry_id']).isRequired,
+    nextPageParamName: PropTypes.oneOf(['page', 'since_entry_id']).isRequired
+  }
+  state = {
+    entries: this.props.entries_info.items,
+    hasMore: this.props.entries_info.has_more,
+    nextPage: this.props.entries_info[this.props.nextPageFieldName],
+    isLoading: false
+  }
   render() {
     return (
       <EntryBricks
-          entries={this.state.entries}
-          loading={this.state.loading}
-          canLoad={!this.state.loading && this.state.hasMore}
-          onLoadMoreEntries={this.loadMoreEntries} />
+        entries={this.state.entries}
+        loading={this.state.isLoading}
+        canLoad={!this.state.isLoading && this.state.hasMore}
+        onLoadMoreEntries={this.loadMoreEntries.bind(this)}
+      />
     );
-  },
-
+  }
   loadMoreEntries() {
-    this.setState({loading: true});
+    const data = {
+      [this.props.nextPageParamName]: this.state.nextPage,
+      limit: this.props.entries_info.limit
+    };
 
-    let { loadUrl, entries_info: { limit } } = this.props;
-    let { sinceEntryID } = this.state;
-
-    EntryActionCreators.load(loadUrl, sinceEntryID, limit)
+    this.setState({ isLoading: true });
+    EntryActionCreators.load(this.props.loadUrl, data)
       .then((entriesInfo) => {
-        if (this.isMounted()) {
-          // Обрабатываем случай, когда передан левый урл. Если в ответе нет нужных
-          // нам полей, просто прекращаем дальнейшую загрузку
-          if (entriesInfo.has_more != null && entriesInfo.next_since_entry_id != null) {
-            this.setState({
-              entries: this.state.entries.concat(entriesInfo.items),
-              hasMore: entriesInfo.has_more,
-              sinceEntryID: entriesInfo.next_since_entry_id
-            });
-          } else {
-            this.setState({hasMore: false});
-          }
+        // Обрабатываем случай, когда передан левый урл. Если в ответе нет нужных
+        // нам полей, просто прекращаем дальнейшую загрузку
+        if (entriesInfo.has_more != null) {
+          this.setState({
+            entries: this.state.entries.concat(entriesInfo.items),
+            hasMore: entriesInfo.has_more,
+            nextPage: entriesInfo[this.props.nextPageFieldName]
+          });
+        } else {
+          this.setState({ hasMore: false });
         }
       })
-      .fail(() => {
-        if (this.isMounted()) this.setState({hasMore: false})
-      })
-      .always(() => {
-        if (this.isMounted()) this.setState({loading: false});
-      });
+      .fail(() => this.setState({ hasMore: false }))
+      .always(() => this.setState({ isLoading: false }))
   }
-});
-
-export default EntryBricksContainer;
+}
