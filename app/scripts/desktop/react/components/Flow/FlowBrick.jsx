b73f810f88ac22df49328af6b457458d1ae0e36c
Aleksey Khoroshev <aleksxor@gmail.com>
2015-09-14 18:59:34 +0700

    [#103166950] clickable flow brick, follow button refactor, disappears when follow

--- app/scripts/desktop/react/components/Flow/FlowBrick.jsx
+++ app/scripts/desktop/react/components/Flow/FlowBrick.jsx
@@ -1,12 +1,15 @@
+import React, { PropTypes } from 'react';
 import classnames from 'classnames';
 import Tooltip from '../common/Tooltip';
-import FollowButton from '../common/FollowButton';
+import FollowButton from '../common/RelationButton/FollowButton';
 import FlowBrickAvatar from './FlowBrickAvatar';
+
+import { REL_NONE_STATE, REL_GUESSED_STATE } from '../common/RelationButton/constants';
 
 let FlowBrick = React.createClass({
   propTypes: {
-    flow: React.PropTypes.object.isRequired,
-    relationship: React.PropTypes.object
+    flow: PropTypes.object.isRequired,
+    relationship: PropTypes.object,
   },
 
   getInitialState() {
@@ -26,50 +29,58 @@ let FlowBrick = React.createClass({
 
     return (
       <article className={brickClasses}>
-        <div className="brick__media">
-          {this.renderFollowButton()}
-          <FlowBrickAvatar flowpic={this.props.flow.flowpic} />
-        </div>
-        <div className="brick__body">
-          <a href={this.props.flow.tlog_url} className="brick__link">
+        <a href="#" onTouchTap={this.onTouchTap}>
+          <div className="brick__media">
+            {this.renderFollowButton()}
+            <FlowBrickAvatar flowpic={this.props.flow.flowpic} />
+          </div>
+          <div className="brick__body">
             <h3 className="brick__title">{this.props.flow.name}</h3>
-          </a>
-          <p className="brick__caption">{this.props.flow.title}</p>
-          <div className="brick__data">
-            <div className="brick__data-item">
-              <Tooltip title={i18n.t('flow_brick.followers_count_tooltip')}>
-                <i className="icon icon--friends" />
-                <span>{this.props.flow.followers_count}</span>
-              </Tooltip>
-            </div>
-            <div className="brick__data-item">
-              <Tooltip title={i18n.t('flow_brick.entries_count_tooltip')}>
-                <i className="icon icon--text-circle" />
-                <span>{this.props.flow.public_tlog_entries_count}</span>
-              </Tooltip>
+            <p className="brick__caption">{this.props.flow.title}</p>
+            <div className="brick__data">
+              <div className="brick__data-item">
+                <Tooltip title={i18n.t('flow_brick.followers_count_tooltip')}>
+                  <i className="icon icon--friends" />
+                  <span>{this.props.flow.followers_count}</span>
+                </Tooltip>
+              </div>
+              <div className="brick__data-item">
+                <Tooltip title={i18n.t('flow_brick.entries_count_tooltip')}>
+                  <i className="icon icon--text-circle" />
+                  <span>{this.props.flow.public_tlog_entries_count}</span>
+                </Tooltip>
+              </div>
             </div>
           </div>
-        </div>
+        </a>
       </article>
     );
   },
 
   renderFollowButton() {
-    if (this.state.relState) {
+    if (this.state.relState &&
+        this.state.relState === REL_NONE_STATE ||
+        this.state.relState === REL_GUESSED_STATE) {
       return (
         <FollowButton
-            objectID={CurrentUserStore.getUserID()}
-            subjectID={this.props.flow.id}
-            subjectPrivacy={this.props.flow.is_privacy}
-            relState={this.state.relState}
-            onChange={this.handleRelStateChange} />
+          objectID={CurrentUserStore.getUserID()}
+          onStateChange={this.handleRelStateChange}
+          relState={this.state.relState}
+          subjectID={this.props.flow.id}
+          subjectPrivacy={this.props.flow.is_privacy}
+        />
       );
     }
   },
 
   handleRelStateChange(relState) {
     this.setState({relState});
-  }
+  },
+
+  onTouchTap(ev) {
+    ev.preventDefault();
+    window.location.href = this.props.flow.tlog_url;
+  },
 });
 
 export default FlowBrick;
