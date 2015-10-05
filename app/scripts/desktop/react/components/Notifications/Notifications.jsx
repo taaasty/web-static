e7c2e302f4d9c8fb0fd65c02ac95423b3888ae18
Aleksey Khoroshev <aleksxor@gmail.com>
2015-10-05 20:45:17 +0700

    [#104397658] mark all notifications as read btn

--- app/scripts/desktop/react/components/Notifications/Notifications.jsx
+++ app/scripts/desktop/react/components/Notifications/Notifications.jsx
@@ -1,48 +1,44 @@
+import React, { PropTypes } from 'react';
 import Scroller from '../common/scroller/scroller';
 import NotificationsNotificationList from './NotificationsNotificationList';
 import NotificationsLoadingMessage from './NotificationsLoadingMessage';
 import NotificationsErrorMessage from './NotificationsErrorMessage';
+import Spinner from '../Spinner';
 
-let Notifications = React.createClass({
-  propTypes: {
-    notifications: React.PropTypes.array.isRequired,
-    loading: React.PropTypes.bool.isRequired,
-    loadingMore: React.PropTypes.bool.isRequired,
-    error: React.PropTypes.bool.isRequired,
-    everythingLoaded: React.PropTypes.bool.isRequired,
-    onNotificationRead: React.PropTypes.func.isRequired,
-    onLoadMore: React.PropTypes.func.isRequired
-  },
+class Notifications {
+  handleScroll(ev) {
+    const { everythingLoaded, loadingMore, onLoadMore } = this.props;
+    if (everythingLoaded || loadingMore) {
+      return;
+    }
 
-  render() {
-    return (
-      <div className="notifications">
-        <Scroller
-            className="scroller--notifications"
-            onScroll={this.handleScroll}>
-          {this.renderContent()}
-        </Scroller>
-      </div>
-    );
-  },
-
+    const el = ev.target;
+    if (el.scrollTop + el.offsetHeight > el.scrollHeight * .9) {
+      onLoadMore();
+    }
+  }
+  onClickMarkAllButton() {
+    this.props.markAllAsRead();
+  }
   renderContent() {
-    if (this.props.loading && this.props.notifications.length == 0) {
+    const { loading, notifications, error, onNotificationRead } = this.props;
+
+    if (loading && notifications.length == 0) {
       return <NotificationsLoadingMessage />;
-    } else if (this.props.error) {
+    } else if (error) {
       return <NotificationsErrorMessage />;
     } else {
       return (
         <div>
           <NotificationsNotificationList
-              notifications={this.props.notifications}
-              onNotificationRead={this.props.onNotificationRead} />
+            notifications={notifications}
+            onNotificationRead={onNotificationRead}
+          />
           {this.renderSpinner()}
         </div>
       );
     }
-  },
-
+  }
   renderSpinner() {
     if (this.props.loadingMore) {
       return (
@@ -51,16 +47,38 @@ let Notifications = React.createClass({
         </div>
       );
     }
-  },
-
-  handleScroll(e) {
-    if (this.props.everythingLoaded || this.props.loadingMore) return;
-
-    let el = e.target;
-    if (el.scrollTop + el.offsetHeight > el.scrollHeight * .9) {
-      this.props.onLoadMore();
-    }
   }
-});
+  renderMarkAllButton() {
+    return this.props.notifications.some((el) => el.read_at === null)
+      ? <div className="notifications__mark-all-read" onClick={this.onClickMarkAllButton.bind(this)}>
+          <span className="icon icon--double-tick" />
+          {i18n.t('buttons.notifications.mark_all_read')}
+        </div>
+      : null;
+  }
+  render() {
+    return (
+      <div className="notifications">
+        {this.renderMarkAllButton()}
+        <Scroller
+          className="scroller--notifications"
+          onScroll={this.handleScroll.bind(this)}
+        >
+          {this.renderContent()}
+        </Scroller>
+      </div>
+    );
+  }
+}
+
+Notifications.propTypes = {
+  error: PropTypes.bool.isRequired,
+  everythingLoaded: PropTypes.bool.isRequired,
+  loading: PropTypes.bool.isRequired,
+  loadingMore: PropTypes.bool.isRequired,
+  notifications: PropTypes.array.isRequired,
+  onLoadMore: PropTypes.func.isRequired,
+  onNotificationRead: PropTypes.func.isRequired,
+};
 
 export default Notifications;
