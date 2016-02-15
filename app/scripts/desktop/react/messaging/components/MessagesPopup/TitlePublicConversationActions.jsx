8c311edbdb3b7e8c29face4118d020b3d721cdee
Aleksey Khoroshev <aleksxor@gmail.com>
2016-02-15 19:19:33 +0700

    [#111591262] move watch action to popup title

--- app/scripts/desktop/react/messaging/components/MessagesPopup/TitlePublicConversationActions.jsx
+++ app/scripts/desktop/react/messaging/components/MessagesPopup/TitlePublicConversationActions.jsx
@@ -2,12 +2,23 @@
 import React, { Component, PropTypes } from 'react';
 import DropdownActions from '../../../components/common/DropdownActions';
 import DropdownAction from '../../../components/common/DropdownAction';
+import ConversationsStore from '../../stores/ConversationsStore';
 import ConversationActions from '../../actions/ConversationActions';
 import MessagesPopupActions from '../../actions/MessagesPopupActions';
 
 class TitlePublicConversationActions extends Component {
+  componentWillMount() {
+    this.syncStateWithStore = () => this.setState({
+      conversation: ConversationsStore.getConversation(this.props.conversation.id),
+    });
+    this.syncStateWithStore();
+    ConversationsStore.addChangeListener(this.syncStateWithStore);
+  }
+  componentWillUnmount() {
+    ConversationsStore.removeChangeListener(this.syncStateWithStore);
+  }
   disturb(flag) {
-    ConversationActions.disturb(this.props.conversation.id, flag);
+    ConversationActions.disturb(this.state.conversation.id, flag);
   }
   startSelect() {
     MessagesPopupActions.startSelect();
@@ -19,7 +30,20 @@ class TitlePublicConversationActions extends Component {
         return MessagesPopupActions.openConversationList();
       });
   }
+  handleClickWatch(ev) {
+    const { id, entry: { is_watching } } = this.state.conversation;
+
+    ev.preventDefault();
+    ev.stopPropagation();
+
+    if (is_watching) {
+      ConversationActions.conversationEntryRemoveFromWatching(id);
+    } else {
+      ConversationActions.conversationEntryAddToWatching(id);
+    }
+  }
   render() {
+    const { can_watch, is_watching } = this.state.conversation.entry;
     const disturb = false;
 
     return (
@@ -31,6 +55,14 @@ class TitlePublicConversationActions extends Component {
             key="dont-disturb"
             onClick={this.disturb.bind(this, !disturb)}
             title={i18n.t('messenger.title_actions.dont_disturb')}
+          />}
+          {can_watch &&
+          <DropdownAction
+            hoverTitle={is_watching && i18n.t('stop_watch_entry_item')}
+            icon="icon--comments-subscribe"
+            key="watch"
+            onClick={this.handleClickWatch.bind(this)}
+            title={i18n.t(is_watching ? 'watching_entry_item' : 'start_watch_entry_item')}
           />}
           <DropdownAction
             icon="icon--double-tick"
