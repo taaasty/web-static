6c8ca73fa35b8636aff21e69cbe9db7b5bc91b0d
Aleksey Khoroshev <aleksxor@gmail.com>
2016-02-18 22:01:48 +0700

    [#108594470] routes (in process)

---
+++ app/scripts/desktop/react/components/FeedPage/index.jsx
@@ -0,0 +1,13 @@
+import React, { Component, PropTypes } from 'react';
+
+class FeedPage extends Component {
+  render() {
+    return <div />;
+  }
+}
+
+FeedPage.propTypes = {
+  
+};
+
+export default FeedPage;
