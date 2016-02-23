a5b91992fd7158d8c848a338bd3422b7d29ffd3b
Aleksey Khoroshev <aleksxor@gmail.com>
2016-02-23 22:17:11 +0700

    [#108594470] feed header title,text,bgimage

--- app/scripts/desktop/react/components/FeedPage/FeedPageHeader.jsx
+++ app/scripts/desktop/react/components/FeedPage/FeedPageHeader.jsx
@@ -1,7 +1,8 @@
+/*global i18n */
 import React, { PropTypes } from 'react';
 
-function FeedPageHeader() {
-  const style = { backgroundImage: 'url("http://taaasty.com/assets/bgs/88/5f/120528_original.jpg")' };
+function FeedPageHeader({ bgImage, text, title }) {
+  const style = bgImage && { backgroundImage: `url(${bgImage})` };
   
   return (
     <header className="page-header">
@@ -11,11 +12,11 @@ function FeedPageHeader() {
           <div className="hero__slide state--current">
             <div className="hero__head">
               <div className="hero__title">
-                Подписки
+                {title}
               </div>
             </div>
             <div className="hero__text">
-              15 друзей пишут
+              {text}
             </div>
           </div>
         </div>
@@ -23,25 +24,27 @@ function FeedPageHeader() {
       <div className="site-apps">
         <img
           className="site-apps__device site-apps__device--iphone"
-          src="/images/site-apps/device-iphone.png" />
+          src="/images/site-apps/device-iphone.png"
+        />
         <img
           className="site-apps__device site-apps__device--android"
-          src="/images/site-apps/device-android.png" />
+          src="/images/site-apps/device-android.png"
+        />
         <div className="site-apps__desc">
           <h5 className="site-apps__title">
-            Установите приложение для iOS или Android
+            {i18n.t('feed.header.install')}
           </h5>
           <div className="site-apps__badges">
             <a
               className="site-apps__badge site-apps__badge--apple-store"
               href="https://itunes.apple.com/us/app/tejsti/id944944503?mt=8"
-              title="Приложение для iOs"
+              title={i18n.t('feed.header.app_ios')}
             >
             </a>
             <a
               className="site-apps__badge site-apps__badge--google-play"
               href="https://play.google.com/store/apps/details?id=ru.taaasty&amp;hl=ru"
-              title="Приложение для Android"
+              title={i18n.t('feed.header.app_android')}
             >
             </a>
           </div>
@@ -54,6 +57,9 @@ function FeedPageHeader() {
 FeedPageHeader.displayName = 'FeedPageHeader';
 
 FeedPageHeader.propTypes = {
+  bgImage: PropTypes.string,
+  text: PropTypes.string,
+  title: PropTypes.string.isRequired,
 };
 
 export default FeedPageHeader;
