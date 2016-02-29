f22b51285c95cfccdaa45edb9c6bfb59406fd3c4
Aleksey Khoroshev <aleksxor@gmail.com>
2016-02-29 18:06:25 +0700

    [#108594470] appstats actions/store/update logic

--- app/scripts/desktop/react/reducers/appStats.js
+++ app/scripts/desktop/react/reducers/appStats.js
@@ -1,20 +1,42 @@
 import createReducer from './createReducer';
+import {
+  APP_STATS_REQUEST,
+  APP_STATS_RECEIVE,
+  APP_STATS_ERROR,
+} from '../actions/AppStatsActions';
 
 const initialState = {
   data: {},
   isFetching: false,
   error: null,
+  updatedAt: null,
 };
 
 const actionMap = {
   [APP_STATS_RECEIVE](state, data) {
-    
+    return {
+      ...state,
+      ...data,
+      isFetching: false,
+      error: null,
+    };
   },
 
   [APP_STATS_REQUEST](state) {
-    
+    return {
+      ...state,
+      isFetching: true,
+      error: null,
+    };
   },
-  
+
+  [APP_STATS_ERROR](state, error) {
+    return {
+      ...state,
+      error,
+      isFetching: false,
+    };
+  },
 };
 
 export default createReducer(initialState, actionMap);
