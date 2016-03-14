e691bed549f0531ea371dac5305e9b1f8fe88c7e
Aleksey Khoroshev <aleksxor@gmail.com>
2016-03-14 16:30:41 +0700

    [#108594470] editor page (in process)

--- app/scripts/desktop/react/components/EditorPage/index.jsx
+++ app/scripts/desktop/react/components/EditorPage/index.jsx
@@ -1,4 +1,8 @@
 import React, { Component, PropTypes } from 'react';
+import { connect } from 'react-redux';
+import EditorNew from '../Editor/EditorNew';
+import Spinner from '../../../../shared/react/components/common/Spinner';
+import { TLOG_SLUG_ANONYMOUS } from '../../../../shared/constants/Tlog';
 
 export const TLOG_TYPE_ANONYMOUS = 'anonymous';
 export const TLOG_TYPE_PUBLIC = 'public';
@@ -6,17 +10,23 @@ export const TLOG_TYPE_PRIVATE = 'private';
 
 class EditorPage extends Component {
   render() {
-    const { design } = this.props.tlog.data;
+    const { data: tlog, isFetching } = this.props.tlog;
+    const tlogType = tlog.slug === TLOG_SLUG_ANONYMOUS
+            ? TLOG_TYPE_ANONYMOUS
+            : tlog.is_privacy ? TLOG_TYPE_PRIVATE : TLOG_TYPE_PUBLIC;
 
     return (
       <div className="page-body">
         <div className="content-area">
-          <div className="content-area__bg" style={{ opacity: design.feedOpacity }} />
+          <div className="content-area__bg" style={{ opacity: tlog.design.feedOpacity }} />
           <div className="content-area__inner">
-            <EditorNew
-              tlog={tlog}
-              tlogType={''}
-            />
+            {isFetching
+             ? <Spinner size={30} />
+             : <EditorNew
+                 tlog={tlog}
+                 tlogType={tlogType}
+               />
+            }
           </div>
         </div>
       </div>
@@ -27,11 +37,11 @@ class EditorPage extends Component {
 EditorPage.displayName = 'EditorPage';
 
 EditorPage.propTypes = {
-  tlog: PropTypes.object.isRequired
+  tlog: PropTypes.object.isRequired,
 };
 
 export default connect(
   (state) => ({
-    tlog: state.tlog.data,
+    tlog: state.tlog,
   })
 )(EditorPage);
