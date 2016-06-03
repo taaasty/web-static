8ec826b2a2fc3bb86ebe4bff5c04d147429cdb0a
Aleksey Khoroshev <aleksxor@gmail.com>
2016-06-03 23:11:34 +0700

    [#120457793] reply actions, stores, form

--- app/scripts/desktop/react/messaging/components/MessagesPopup/Thread/MessageList/Item.jsx
+++ app/scripts/desktop/react/messaging/components/MessagesPopup/Thread/MessageList/Item.jsx
@@ -1,15 +1,13 @@
-/*global i18n, ReactGrammarMixin */
+/*global ReactGrammarMixin */
 import React, { createClass, PropTypes } from 'react';
 import classnames from 'classnames';
 import moment from 'moment';
-import ImgFromFile from '../ImgFromFile';
 import MsgUserAvatar from '../../MsgUserAvatar';
-import Image from '../../../../../../../shared/react/components/common/Image';
 import { browserHistory } from 'react-router';
 import uri from 'urijs';
-import Tooltip from '../../../../../components/common/Tooltip';
 import { SUPPORT_ID } from '../../../../../components/SupportLauncher';
 import { PRIVATE_CONVERSATION } from '../../../../constants/ConversationConstants';
+import MessageContents from './MessageContents';
 
 const ERROR_STATE = 'error';
 const SENT_STATE = 'sent';
@@ -55,12 +53,8 @@ const Item = createClass({
   },
 
   handleClickUser(ev) {
-    const { user } = this.props.messageInfo;
-
-    if (window.SPA) {
-      ev.preventDefault();
-      browserHistory.push({ pathname: uri(user.tlog_url).path() });
-    }
+    ev.preventDefault();
+    browserHistory.push({ pathname: uri(this.props.messageInfo.user.tlog_url).path() });
   },
 
   renderDeliveryStatus() {
@@ -87,45 +81,6 @@ const Item = createClass({
     return created_at && moment(created_at).format('D MMMM HH:mm');
   },
 
-  renderAttachments() {
-    const { attachments, files } = this.props.message;
-
-    return attachments && attachments.length
-      ? attachments.map((img) => (
-          <div className="messages__img">
-            <a href={img.url} target="_blank">
-              <Image
-                image={img}
-                isRawUrl
-                maxWidth={220}
-              />
-            </a>
-          </div>))
-      : files && files.length
-        ? files.map((file) => (
-            <div className="messages__img">
-              <ImgFromFile file={file} />
-            </div>))
-        : null;
-  },
-
-  renderUserSlug() {
-    const { slug, tlog_url } = this.props.messageInfo.user;
-
-    return this.isIncoming()
-      ? <span className="messages__user-name">
-          <a
-            href={tlog_url}
-            onClick={this.handleClickUser}
-            target="_blank"
-          >
-            {slug}
-            {this.isSupport() && this.renderSupportIcon()}
-          </a>
-        </span>
-      : null;
-  },
-
   renderUserAvatar(user) {
     return (
       <a
@@ -140,49 +95,16 @@ const Item = createClass({
     );
   },
 
-  renderSupportIcon() {
-    const { user } = this.props.messageInfo;
-    const { browser, platform } = this.props.message;
-    const { gender, locale, id, email, is_privacy, is_premium,
-            has_design_bundle, created_at } = user;
-
-    const support_info = i18n.t('messenger.support_info', {
-      browser,
-      platform,
-      locale,
-      id,
-      email,
-      gender: i18n.t('gender', { context: gender }),
-      private: i18n.t(is_privacy ? 'yes' : 'no'),
-      premium: i18n.t(is_premium ? 'yes' : 'no'),
-      design_bundle: i18n.t(has_design_bundle ? 'yes' : 'no'),
-      registered_at: moment(created_at).format('LL'),
-    });
-
-    return (
-      <Tooltip
-        container=".messages"
-        html
-        placement="top"
-        title={support_info}
-      >
-        <i className="icon icon--exclamation-mark message-support-info" />
-      </Tooltip>
-    );
-  },
-
   renderAvatar() {
     const { user } = this.props.messageInfo;
 
-    if (!this.isIncoming()) {
-      return null;
-    } else if (this.props.conversationType !== PRIVATE_CONVERSATION) {
-      return this.renderUserAvatar(user);
-    }
+    return (this.isIncoming() && this.props.conversationType !== PRIVATE_CONVERSATION)
+      ? this.renderUserAvatar(user)
+      : null;
   },
 
   render() {
-    const { message: { content_html }, selected } = this.props;
+    const { message, messageInfo, selected } = this.props;
     const messageClasses = classnames({
       'message': true,
       'message--from': this.isOutgoing(),
@@ -198,14 +120,12 @@ const Item = createClass({
         <div className={messageClasses}>
           {this.renderAvatar()}
           <div className="messages__bubble">
-            {this.renderUserSlug()}
-            <span
-              className="messages__text"
-              dangerouslySetInnerHTML={{__html: content_html || ''}}
+            <MessageContents
+              message={message}
+              messageInfo={messageInfo}
+              showSlug={this.isIncoming()}
+              showSupportInfo={this.isSupport()}
             />
-            <div className="messages__img-container">
-              {this.renderAttachments()}
-            </div>
           </div>
           <span className="messages__date">
             {this.renderMessageCreatedAt()}
