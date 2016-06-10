37d1a902ff2123ca826b0195b7ecddc5f88b2b75
Aleksey Khoroshev <aleksxor@gmail.com>
2016-06-10 15:51:53 +0700

    [#119815405, #119647263] featured users block

--- app/scripts/desktop/react/components/PeoplePage/PeopleList.jsx
+++ app/scripts/desktop/react/components/PeoplePage/PeopleList.jsx
@@ -3,18 +3,32 @@ import PeopleItem from './PeopleItem';
 import Spinner from '../../../../shared/react/components/common/Spinner';
 
 function PeopleList({ isFetching, people, query }) {
+  function renderContents() {
+    if (isFetching) {
+      return (
+        <div className="people__msg-container">
+          <Spinner size={30} />
+        </div>
+      );
+    } else if (people.length === 0) {
+      return (
+        <div className="people__msg-container">
+          {query ? i18n.t('people.query_empty', { query }) : i18n.t('people.empty')}
+        </div>
+      ); 
+    } else {
+      return people.map(({ title, user }, idx) => (
+        <PeopleItem
+          key={`user-${idx}`}
+          title={title}
+          user={user}
+        />));
+    }
+  }
+
   return (
    <section className="people">
-     {isFetching
-      ? <Spinner size={30} />
-      : people.length === 0
-        ? query ? i18n.t('people.query_empty', { query }) : i18n.t('people.empty')
-        : people.map(({ title, user }, idx) => (
-            <PeopleItem
-              key={`user-${idx}`}
-              title={title}
-              user={user}
-            />))}
+     {renderContents()}
    </section>
   );
 }
