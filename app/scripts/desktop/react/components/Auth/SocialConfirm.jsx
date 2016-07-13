aae4ba0ccf140495c276c85c61091013eb9325be
Aleksey Khoroshev <aleksxor@gmail.com>
2016-07-13 12:30:07 +0700

    [#125232681] terms link on register confirm page

--- app/scripts/desktop/react/components/Auth/SocialConfirm.jsx
+++ app/scripts/desktop/react/components/Auth/SocialConfirm.jsx
@@ -1,6 +1,7 @@
 /*global i18n, ReactShakeMixin */
 import React, { PropTypes, createClass } from 'react';
 import ConfirmRegistrationMixin from './ConfirmRegistrationMixin';
+import Routes from '../../../../shared/routes/routes';
 
 const SocialNetworksConfirmRegistration = createClass({
   propTypes: {
@@ -23,12 +24,22 @@ const SocialNetworksConfirmRegistration = createClass({
         <div className="form-popup__body">
           <form action={postUrl} method="post">
             <div className="form-popup__lead">
-              {i18n.t('confirm_signup', { userSlug: slug })}
+              {i18n.t('auth.confirm_signup', { userSlug: slug })}
             </div>
             <div className="form-popup__submit">
               <button className="button button--large button--green-light button--block button--rectangle">
-                {i18n.t('confirm_signup_approve')}
+                {i18n.t('auth.confirm_signup_approve')}
               </button>
+            </div>
+            <div className="form-popup__terms">
+              {i18n.t('auth.accept_terms')}
+              <a
+                className="form-popup__terms-link"
+                href={Routes.terms()}
+                target="_blank"
+              >
+                {i18n.t('auth.accept_terms_link')}
+              </a>
             </div>
           </form>
         </div>
@@ -37,7 +48,7 @@ const SocialNetworksConfirmRegistration = createClass({
             {i18n.t('or')}
           </span>
           <a className="form-popup__footer-item" onClick={this.handleDisapproveClick}>
-            {i18n.t('already_registered_link')}
+            {i18n.t('auth.already_registered_link')}
           </a>
         </div>
       </div>
